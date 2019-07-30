a24bf196211985e332b3bdc1baa9f18202320353
Kanwar Saad Bin Liaqat <kanwar.saad.bin.liaqat@est.tech>
2019-07-30 16:23:57 +0300

    Openstack Infra utility functions
    This commit contains openstack
    utility functions primarily required
    for CI and Dev infrastructure.

---
+++ ci/scripts/openstack/infra_defines.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+
+# shellcheck disable=SC2034
+true
+
+# External network for Airship Openstack Tenant
+EXT_NET="ext-net"
+
+# Global defines for Airship CI infrastructure
+# ============================================
+
+CI_ROUTER_NAME="airship-ci-ext-router"
+CI_EXT_NET="airship-ci-ext-net"
+CI_EXT_SUBNET_CIDR="10.100.10.0/24"
+CI_INT_NET="airship-ci-int-net"
+CI_INT_SUBNET_CIDR="10.0.10.0/24"
+
+# Global defines for Airship DEV infrastructure
+# ============================================
+
+DEV_ROUTER_NAME="airship-dev-ext-router"
+DEV_EXT_NET="airship-dev-ext-net"
+DEV_EXT_SUBNET_CIDR="10.101.10.0/24"
+DEV_INT_NET="airship-dev-int-net"
+DEV_INT_SUBNET_CIDR="10.1.10.0/24"
+
+
---
+++ ci/scripts/openstack/utils.sh
@@ -0,0 +1,373 @@
+#! /usr/bin/env bash
+
+# Description:
+# Extract IP from CIDR
+#
+# Example:
+#   Input: "10.0.10.0/24"
+#   Output: "10.0.10.0"
+#
+get_ip_from_cidr() {
+  echo "${1:?}" | cut -d'/' -f1
+}
+
+# Description:
+# Extract Prefix from CIDR
+#
+# Example:
+#   Input: "10.0.10.0/24"
+#   Output: "24"
+#
+get_prefix_from_cidr() {
+  echo "${1:?}" | cut -d'/' -f2
+}
+
+# Description:
+# Get N-th IP after given IP
+#
+# Example:
+#   Input: "10.0.10.0" 15
+#   Output: "10.0.10.15"
+#
+get_nth_ip(){
+  local IP INCREMENT IP_HEX NEXT_IP_HEX NEXT_IP
+
+  IP="${1:?}"
+  INCREMENT="${2:-1}"
+  # shellcheck disable=SC2183
+  # shellcheck disable=SC2086
+  IP_HEX="$(printf '%.2X%.2X%.2X%.2X\n' ${IP//./ })"
+  NEXT_IP_HEX="$(printf %.8X "$(( 0x${IP_HEX} + INCREMENT ))")"
+  # shellcheck disable=SC2183
+  # shellcheck disable=SC2046
+  NEXT_IP="$(printf '%d.%d.%d.%d\n' $(echo "${NEXT_IP_HEX}" | sed -r 's/(..)/0x\1 /g') )"
+  echo "${NEXT_IP}"
+}
+
+# Description:
+# Get Gateway IP address from CIDR.
+# In our scenario we always define 1st IP in
+# CIDR as Gateway.
+#
+# Example:
+#   Input: "10.0.10.0/24"
+#   Output: "10.0.10.1"
+#
+get_gateway_ip_from_cidr() {
+  get_nth_ip "$(get_ip_from_cidr "${1:?}")" 1
+}
+
+# Description:
+# Generates subnet name from Network name.
+#
+# Example:
+#   Input: "airship-network"
+#   Output: "airship-network-subnet"
+#
+get_subnet_name() {
+  echo "${1:?}-subnet"
+}
+
+# Description:
+# Generates External Port name from Network name.
+#
+# Example:
+#   Input: "airship-network"
+#   Output: "airship-network-ext-port"
+#
+get_external_port_name() {
+  echo "${1:?}-ext-port"
+}
+
+# Description:
+# Returns a list of all ports in
+# an openstack subnet
+#
+# Usage: get_ports_in_subnet <subnet_name_or_id>
+#
+get_ports_in_subnet() {
+  local SUBNET PORT_LIST
+
+  SUBNET="${1:?}"
+
+  # Get Port List
+  PORT_LIST="$(openstack port list --fixed-ip subnet="${SUBNET}" -f json)"
+
+  [[ -z "${PORT_LIST}" ]] && return
+
+  # Output list of IDs
+  echo "${PORT_LIST}" | jq -r 'map(.ID) | @csv' | tr ',' '\n' | tr -d '"'
+}
+
+# Description:
+# Returns a list of all subnet in
+# an openstack network
+#
+# Usage: get_subnets_in_network <network_name_or_id>
+#
+get_subnets_in_network() {
+  local NET SUBNET_LIST
+
+  NET="${1:?}"
+
+  # Get Port List
+  SUBNET_LIST="$(openstack subnet list --network "${NET}" -f json)"
+
+  [[ -z "${SUBNET_LIST}" ]] && return
+
+  # Output list of IDs
+  echo "${SUBNET_LIST}" | jq -r 'map(.ID) | @csv' | tr ',' '\n' | tr -d '"'
+}
+
+# Description:
+# Deletes an openstack port.
+#
+# Usage: delete_port <port_name_or_id>
+#
+delete_port() {
+  local PORT PORT_INFO DEVICE_OWNER DEVICE_ID
+
+  PORT="${1:?}"
+
+  # Get port info
+  PORT_INFO="$(openstack port show -f json "${PORT}" || true)"
+
+  [[ -z "${PORT_INFO}" ]] && return
+
+  # Get device info
+  DEVICE_OWNER="$(echo "${PORT_INFO}" | jq -r '.device_owner')"
+
+
+  # if connected to router then disconnect from router
+  if [[ "${DEVICE_OWNER}" = *router* ]]
+  then
+    DEVICE_ID="$(echo "${PORT_INFO}" | jq -r '.device_id')"
+    echo "deleting port ${PORT} from device ${DEVICE_ID}"
+    openstack router remove port "${DEVICE_ID}" "${PORT}" > /dev/null
+  else
+    # delete port
+    echo "deleting port ${PORT}"
+    openstack port delete "${PORT}" > /dev/null
+  fi
+}
+
+# Description:
+# Deletes an openstack subnet. It deletes
+# all associated ports before deleting the subnet.
+#
+# Usage: delete_subnet <subnet_name_or_id>
+#
+delete_subnet() {
+  local SUBNET SUBNET_INFO PORTS
+
+  SUBNET="${1:?}"
+
+  # Get Subnet info
+  SUBNET_INFO="$(openstack subnet show -f json "${SUBNET}" || true)"
+
+  [[ -z "${SUBNET_INFO}" ]] && return
+
+  # Get all ports in this subnet
+  PORTS="$(get_ports_in_subnet "${SUBNET}")"
+
+  # Delete ports
+  for PORT in ${PORTS}
+  do
+    delete_port "${PORT}"
+  done
+
+  # delete subnet
+  echo "deleting subnet ${SUBNET}"
+  openstack subnet delete "${SUBNET}" > /dev/null
+}
+
+# Description:
+# Deletes an openstack network. Deletes
+# any associated subnets and ports before
+# deleting network. If the network is deleted by
+# name then there can be multiple networks
+# by that name. This function will delete all
+# the networks by that name and their associated
+# subnets and ports.
+#
+# Usage: delete_port <network_name_or_id>
+#
+delete_network() {
+  local NET NET_LIST_ALL JQ_QUERY NET_LIST NET_ARRAY NET_INFO SUBNETS
+
+  NET="${1:?}"
+
+  # Get list of networks with name
+  NET_LIST_ALL="$(openstack network list -f json || true)"
+  JQ_QUERY="map(select((.Name | contains(\"${NET}\")) or (.ID | contains(\"${NET}\"))))"
+  NET_LIST="$(echo "${NET_LIST_ALL}" | jq "${JQ_QUERY}")"
+  NET_ARRAY="$(echo "${NET_LIST}" | jq -r 'map(.ID) | @csv' | tr ',' '\n' | tr -d '"')"
+
+  for NET_ID in ${NET_ARRAY}
+  do
+    # Get Subnet info
+    NET_INFO="$(openstack network show -f json "${NET_ID}" || true)"
+
+    [[ -z "${NET_INFO}" ]] && return
+
+    # Get list of subnet IDs in this network
+    SUBNETS="$(get_subnets_in_network "${NET_ID}")"
+
+    # Delete subnets
+    for SUBNET in ${SUBNETS}
+    do
+      delete_subnet "${SUBNET}"
+    done
+
+    # delete network
+    echo "deleting network ${NET_ID}"
+    openstack network delete "${NET_ID}" > /dev/null
+  done
+}
+
+# Description:
+# Creates a network and a default subnet.
+# If IS_EXTERNAL is 1 then also sets a gateway
+# for subnet
+#
+# Usage:
+#   create_network_and_subnet <cidr> <is_external:0/1> <network_name>
+#
+create_network_and_subnet() {
+
+  local CIDR NET_NAME SUBNET_NAME IS_EXTERNAL NET_INFO
+  local GATEWAY_IP START_IP END_IP NET_ID GATEWAY_KEYWORD GATEWAY
+  local SUBNET_INFO SUBNET_ID
+
+  CIDR="${1:?}"
+  IS_EXTERNAL="${2:?}"
+  NET_NAME="${3:?}"
+
+  SUBNET_NAME="$(get_subnet_name "${NET_NAME}")"
+  GATEWAY_IP="$(get_gateway_ip_from_cidr "${CIDR}")"
+  START_IP="$(get_nth_ip "${GATEWAY_IP}" 9)"
+  END_IP="$(get_nth_ip "${GATEWAY_IP}" 199)"
+
+  NET_INFO="$(openstack network create -f json\
+    --enable \
+    --description "${NET_NAME}" \
+    --disable-port-security \
+    "${NET_NAME}")"
+
+  NET_ID="$(echo "${NET_INFO}" | jq -r '.id')"
+  GATEWAY_KEYWORD=$([ "${IS_EXTERNAL}" = 1 ] && echo "--gateway")
+  GATEWAY=$([ "${IS_EXTERNAL}" = 1 ] && echo "${GATEWAY_IP}")
+
+  SUBNET_INFO="$(openstack subnet create -f json\
+    --subnet-range "${CIDR}" \
+    --dhcp \
+    --ip-version 4 \
+    --network "${NET_ID}" \
+    "${GATEWAY_KEYWORD}" "${GATEWAY}" \
+    --allocation-pool start="${START_IP}",end="${END_IP}" \
+    "${SUBNET_NAME}")"
+
+  SUBNET_ID="$(echo "${SUBNET_INFO}" | jq -r '.id')"
+
+  echo "${NET_ID}" "${SUBNET_ID}"
+}
+
+# Description:
+# Creates a network and attaches it to router for
+# external connectivity.
+#
+# Usage:
+#   create_external_net <cidr> <router_name_or_id> <network_name>
+#
+create_external_net() {
+  local CIDR NET_NAME SUBNET_NAME ROUTER PORT_NAME
+  local GATEWAY_IP NET_INFO NET_ID SUBNET_ID PORT_INFO PORT_ID
+
+  CIDR="${1:?}"
+  ROUTER="${2:?}"
+  NET_NAME="${3:?}"
+
+  SUBNET_NAME="$(get_subnet_name "${NET_NAME}")"
+  PORT_NAME="$(get_external_port_name "${NET_NAME}")"
+  GATEWAY_IP="$(get_gateway_ip_from_cidr "${CIDR}")"
+
+  NET_INFO="$(create_network_and_subnet "${CIDR}" 1 "${NET_NAME}")"
+
+  NET_ID="$(echo "${NET_INFO}" | cut -d' ' -f1)"
+  SUBNET_ID="$(echo "${NET_INFO}" | cut -d' ' -f2)"
+
+  # Create external Port
+  PORT_INFO="$(openstack port create -f json \
+    --network "${NET_ID}" \
+    --fixed-ip subnet="${SUBNET_ID}",ip-address="${GATEWAY_IP}" \
+    "${PORT_NAME}")"
+
+  PORT_ID="$(echo "${PORT_INFO}" | jq -r '.id')"
+
+  # Attach port to router
+  openstack router add port "${ROUTER}" "${PORT_ID}" > /dev/null
+}
+
+# Description:
+# Creates a Router and attaches it to external gateway.
+#
+# Usage:
+#   create_router <external_network> <router_name>
+#
+create_router() {
+  local CIDR NET_NAME SUBNET_NAME ROUTER PORT_NAME
+  local GATEWAY_IP NET_INFO NET_ID SUBNET_ID
+
+  EXT_NET="${1:?}"
+  ROUTER_NAME="${2:?}"
+
+  ROUTER_INFO="$(openstack router create -f json\
+    --enable \
+    --description "${ROUTER_NAME}" \
+    --ha \
+    "${ROUTER_NAME}")"
+
+  ROUTER_ID="$(echo "${ROUTER_INFO}" | jq -r '.id')"
+
+  openstack router set --external-gateway "${EXT_NET}" "${ROUTER_ID}" > /dev/null
+}
+
+# Description:
+# Deletes a Router and any associated ports attached to it.
+#
+# Usage:
+#   delete_router <router_name>
+#
+delete_router() {
+  local ROUTER_LIST_ALL JQ_QUERY ROUTER_LIST ROUTER_ARRAY
+  local ROUTER_NAME ROUTER_INFO ROUTER_ID ROUTER_PORT_INFO ROUTER_PORT_ID_LIST
+
+  ROUTER_NAME="${1:?}"
+
+  # Get list of networks with name
+  ROUTER_LIST_ALL="$(openstack router list -f json || true)"
+  JQ_QUERY="map(select((.Name | contains(\"${ROUTER_NAME}\")) or (.ID | contains(\"${ROUTER_NAME}\"))))"
+  ROUTER_LIST="$(echo "${ROUTER_LIST_ALL}" | jq "${JQ_QUERY}")"
+  ROUTER_ARRAY="$(echo "${ROUTER_LIST}" | jq -r 'map(.ID) | @csv' | tr ',' '\n' | tr -d '"')"
+
+  for ROUTER_ID in ${ROUTER_ARRAY}
+  do
+    ROUTER_INFO="$(openstack router show "${ROUTER_ID}" -f json)"
+
+    [[ -z "${ROUTER_INFO}" ]] && return
+
+    ROUTER_PORT_INFO="$(echo "${ROUTER_INFO}" | jq -r '.interfaces_info')"
+    ROUTER_PORT_ID_LIST="$(echo "${ROUTER_PORT_INFO}" \
+      | sed 's@\\@@g' \
+      | jq -r 'map(.port_id) | @csv' \
+      | tr ',' '\n' \
+      | tr -d '"')"
+
+    for PORT in ${ROUTER_PORT_ID_LIST}
+    do
+      delete_port "${PORT}"
+    done
+
+    openstack router delete "${ROUTER_ID}" > /dev/null
+  done
+}
