3a731fa3302b4fd765635d8e48a6524e1f79d084
maelk <mael.kimmerlin@est.tech>
2019-08-01 16:40:56 +0300

    Add provisioning script for Metal3-dev-env
    Clones the metal3-dev-env repo and execute the
    01_install_requirements.sh script
    Add the Metal3 image building script

---
+++ ci/scripts/provision_metal3_image.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -uex
+
+# Metal3 Dev Env variables
+M3_DENV_ORG="${M3_DENV_ORG:-metal3-io}"
+M3_DENV_REPO="${M3_DENV_REPO:-metal3-dev-env}"
+M3_DENV_URL="${M3_DENV_URL:-https://github.com/${M3_DENV_ORG}/${M3_DENV_REPO}.git}"
+M3_DENV_BRANCH="${M3_DENV_BRANCH:-master}"
+M3_DENV_ROOT="${M3_DENV_ROOT:-/tmp}"
+M3_DENV_PATH="${M3_DENV_PATH:-${M3_DENV_ROOT}/${M3_DENV_REPO}}"
+FORCE_REPO_UPDATE="${FORCE_REPO_UPDATE:-true}"
+
+export CONTAINER_RUNTIME="${CONTAINER_RUNTIME:-docker}"
+
+sudo apt install -y git
+
+mkdir -p "${M3_DENV_ROOT}"
+if [[ -d "${M3_DENV_PATH}" && "${FORCE_REPO_UPDATE}" == "true" ]]; then
+  sudo rm -rf "${M3_DENV_PATH}"
+fi
+if [ ! -d "${M3_DENV_PATH}" ] ; then
+  pushd "${M3_DENV_ROOT}"
+  git clone "${M3_DENV_URL}"
+  popd
+fi
+pushd "${M3_DENV_PATH}"
+git checkout "${M3_DENV_BRANCH}"
+git pull -r || true
+"${M3_DENV_PATH}/01_install_requirements.sh"
+popd
+
+rm -rf "${M3_DENV_PATH}"
