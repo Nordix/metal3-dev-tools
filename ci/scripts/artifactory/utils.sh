07ba4adc7e68a2650b0e77181bdb6279d07e4781
Kanwar Saad Bin Liaqat <kanwar.saad.bin.liaqat@est.tech>
2019-08-06 12:44:06 +0300

    ci: Add/Del jumphost SSH keys
    Scripts for adding and removing user ssh keys
    from artifactory.
    
    - added usage comments in different scripts

--- ci/scripts/artifactory/utils.sh
+++ ci/scripts/artifactory/utils.sh
@@ -2,6 +2,18 @@
 
 # ================ Generic Artifactory Helper Functions ===============
 
+# Description:
+# Adds a new artifact in artifactory at a given path.
+#
+# Anonymous flag is set to perform all operations anonymously.
+# If anonymous flag is not set then credentials should be set
+# in the following environment variables.
+#   RT_USER: artifactory user name.
+#   RT_TOKEN: artifactory password or api token
+#
+# Usage:
+#   rt_upload_artifact <src_file_path> <dest_file_path> <anonymous:0/1>
+#
 rt_upload_artifact() {
   SRC_PATH="${1:?}"
   DST_PATH="${2:?}"
@@ -15,6 +27,18 @@ rt_upload_artifact() {
   eval "${_CMD}"
 }
 
+# Description:
+# Download artifact from artifactory.
+#
+# Anonymous flag is set to perform all operations anonymously.
+# If anonymous flag is not set then credentials should be set
+# in the following environment variables.
+#   RT_USER: artifactory user name.
+#   RT_TOKEN: artifactory password or api token
+#
+# Usage:
+#   rt_download_artifact <src_file_path> <dest_file_path> <anonymous:0/1>
+#
 rt_download_artifact() {
   SRC_PATH="${1:?}"
   DST_PATH="${2:?}"
@@ -29,6 +53,18 @@ rt_download_artifact() {
   eval "${_CMD}"
 }
 
+# Description:
+# Download artifact from artifactory and dumps it on stdout.
+#
+# Anonymous flag is set to perform all operations anonymously.
+# If anonymous flag is not set then credentials should be set
+# in the following environment variables.
+#   RT_USER: artifactory user name.
+#   RT_TOKEN: artifactory password or api token
+#
+# Usage:
+#   rt_cat_artifact <src_file_path> <anonymous:0/1>
+#
 rt_cat_artifact() {
   SRC_PATH="${1:?}"
   ANONYMOUS="${2:-1}"
@@ -41,6 +77,18 @@ rt_cat_artifact() {
   eval "${_CMD}"
 }
 
+# Description:
+# Delete artifact from artifactory.
+#
+# Anonymous flag is set to perform all operations anonymously.
+# If anonymous flag is not set then credentials should be set
+# in the following environment variables.
+#   RT_USER: artifactory user name.
+#   RT_TOKEN: artifactory password or api token
+#
+# Usage:
+#   rt_cat_artifact <dst_file_path> <anonymous:0/1>
+#
 rt_delete_artifact() {
   DST_PATH="${1:?}"
   ANONYMOUS="${2:-1}"
@@ -50,9 +98,22 @@ rt_delete_artifact() {
     -XDELETE \
     ${RT_URL}/${DST_PATH}"
 
-  eval "${_CMD}"
+  eval "${_CMD}" > /dev/null 2>&1
 }
 
+# Description:
+# Lists a directory in artifactory. The result is the json
+# dump of artifactory response.
+#
+# Anonymous flag is set to perform all operations anonymously.
+# If anonymous flag is not set then credentials should be set
+# in the following environment variables.
+#   RT_USER: artifactory user name.
+#   RT_TOKEN: artifactory password or api token
+#
+# Usage:
+#   rt_list_directory <dst_dir_path> <anonymous:0/1>
+#
 rt_list_directory() {
   DST_PATH="${1:?}"
   ANONYMOUS="${2:-1}"
@@ -70,11 +131,18 @@ rt_list_directory() {
 RT_AIRSHIP_DIR="airship"
 RT_USERS_DIR="${RT_AIRSHIP_DIR}/users"
 
+# Description:
+# Gets all the keys for a user from airship/users/<username>
+# directory.
+#
+# Usage:
+#   rt_get_user_public_keys <username>
+#
 rt_get_user_public_keys() {
 
   USER="${1:?}"
   USER_KEY_FILES="$(rt_list_directory "${RT_USERS_DIR}/${USER}" \
-    | jq -r '.children[] |select(.folder==false) |.uri')"
+    | jq -r '.children[]? |select(.folder==false) |.uri')" > /dev/null 2>&1
 
   USER_KEYS=""
   for PUB_KEY_FILE in ${USER_KEY_FILES}
@@ -86,6 +154,16 @@ rt_get_user_public_keys() {
   echo "${USER_KEYS}"
 }
 
+# Description:
+# Adds a user public key in airship/users/<username>/<key_name>
+#
+# Credentials should be set in the following environment variables.
+#   RT_USER: artifactory user name.
+#   RT_TOKEN: artifactory password or api token
+#
+# Usage:
+#   rt_add_user_public_key <user> <public_key_name> <public_key>
+#
 rt_add_user_public_key() {
 
   USER="${1:?}"
@@ -98,10 +176,34 @@ rt_add_user_public_key() {
   rt_upload_artifact "${USER_PUB_KEY_FILE}" "${RT_USERS_DIR}/${USER}/${USER_PUB_KEY_NAME}" 0
 }
 
+# Description:
+# Deletes a user public key in airship/users/<username>/<key_name>
+#
+# Credentials should be set in the following environment variables.
+#   RT_USER: artifactory user name.
+#   RT_TOKEN: artifactory password or api token
+#
+# Usage:
+#   rt_del_user_public_key <user> <public_key_name>
+#
 rt_del_user_public_key() {
 
   USER="${1:?}"
   USER_PUB_KEY_NAME="${2:?}"
 
-  rt_delete_artifact "${RT_USERS_DIR}/${USER}/${USER_PUB_KEY_NAME}" 0
+  USER_KEY_STAT="$(rt_list_directory "${RT_USERS_DIR}/${USER}/${USER_PUB_KEY_NAME}" \
+    | jq -r '.children[]? |select(.folder==false) |.uri')" > /dev/null 2>&1
+
+  if [ -n "${USER_KEY_STAT}" ]
+  then
+    rt_delete_artifact "${RT_USERS_DIR}/${USER}/${USER_PUB_KEY_NAME}" 0
+  fi
+
+  # Delete the user directory if there are no more keys
+  KEYS="$(rt_get_user_public_keys "test")"
+
+  if [ -z "${KEYS}" ]
+  then
+    rt_delete_artifact "${RT_USERS_DIR}/${USER}" 0
+  fi
 }
