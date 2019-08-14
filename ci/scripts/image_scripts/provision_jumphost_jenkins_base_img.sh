dd88ae0518dcb60783de8aabf98a9ea3fd5c4e78
maelk <mael.kimmerlin@est.tech>
2019-08-14 09:50:26 +0300

    Disable unattended upgrades in image to avoid apt lock
    and fix variable name issue in integration tests script

--- ci/scripts/image_scripts/provision_jumphost_jenkins_base_img.sh
+++ ci/scripts/image_scripts/provision_jumphost_jenkins_base_img.sh
@@ -7,19 +7,6 @@ set -eux
 
 SCRIPTS_DIR="$(dirname "$(readlink -f "${0}")")"
 
-# Wait for package manager lock to be released
-
-sudo killall apt apt-get || true
-sudo rm -f /var/lib/dpkg/lock-frontend
-sudo rm -f /var/lib/dpkg/lock
-sudo rm -f /var/lib/apt/lists/lock
-sudo rm -f /var/cache/apt/archives/lock
-
-until sudo dpkg --configure -a
-do
-  sleep 1
-done
-
 # Install required packages.
 sudo apt install -y \
   openjdk-8-jre
