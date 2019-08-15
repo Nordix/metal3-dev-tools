c9ff75fd8fe98c8eb0823d0b155e1f54d512b4fc
Mika Koskimaki <mika.koskimaki@est.tech>
2019-08-15 14:59:38 +0300

    ci: add metal3 integration test pipeline

--- ci/scripts/tests/integration_test.sh
+++ ci/scripts/tests/integration_test.sh
@@ -26,9 +26,30 @@ source "${OS_SCRIPTS_DIR}/utils.sh"
 # shellcheck disable=SC1090
 source "${OS_SCRIPTS_DIR}/infra_defines.sh"
 
-TEST_EXECUTER_VM_NAME="${TEST_EXECUTER_VM_NAME:-ci-int-tests-vm}"
+VM_TIMELABEL="${VM_TIMELABEL:-$(date '+m%d%H%M%S')}"
+TEST_EXECUTER_VM_NAME="${TEST_EXECUTER_VM_NAME:-ci-tests-vm-${VM_TIMELABEL}}"
 TEST_EXECUTER_PORT_NAME="${TEST_EXECUTER_PORT_NAME:-${TEST_EXECUTER_VM_NAME}-int-port}"
 TEST_EXECUTER_FLAVOR="${TEST_EXECUTER_FLAVOR:-4C-16GB-50GB}"
+
+
+REPO_ORG="${REPO_ORG:-metal3-io}"
+REPO_NAME="${REPO_NAME:-metal3-dev-env}"
+REPO_BRANCH="${REPO_BRANCH:-master}"
+UPDATED_REPO="https://github.com/${REPO_ORG}/${REPO_NAME}.git"
+
+if [ "${REPO_NAME}" == "metal3-dev-env" ]
+then
+   METAL3REPO="${UPDATED_REPO}"
+   METAL3BRANCH="${REPO_BRANCH}"
+elif [ "${REPO_NAME}" == "baremetal-operator" ]
+then
+   BMOREPO="${UPDATED_REPO}"
+   BMOBRANCH="${REPO_BRANCH}"
+elif [ "${REPO_NAME}" == "cluster-api-provider-baremetal" ]
+then
+   CAPBMREPO="${UPDATED_REPO}"
+   CAPBMBRANCH="${REPO_BRANCH}"
+fi
 
 METAL3REPO="${METAL3REPO:-https://github.com/metal3-io/metal3-dev-env.git}"
 METAL3BRANCH="${METAL3BRANCH:-master}"
@@ -57,7 +78,7 @@ TEST_EXECUTER_IP="$(openstack port show -f json "${TEST_EXECUTER_PORT_NAME}" \
   | jq -r '.fixed_ips' | tr ',' '\n' | grep ip_address | cut -d '=' -f2- \
   | tr -d "'")"
 
-echo "Waiting for the host to come up"
+echo "Waiting for the host ${TEST_EXECUTER_VM_NAME} to come up"
 #Wait for the host to come up
 wait_for_ssh "${AIRSHIP_CI_USER}" "${AIRSHIP_CI_USER_KEY}" "${TEST_EXECUTER_IP}"
 
@@ -75,6 +96,7 @@ echo "Running the tests"
 ssh \
   -o StrictHostKeyChecking=no \
   -o UserKnownHostsFile=/dev/null \
+  -o ServerAliveInterval=15 \
   -i "${AIRSHIP_CI_USER_KEY}" \
   "${AIRSHIP_CI_USER}"@"${TEST_EXECUTER_IP}" \
   /tmp/run_integration_tests.sh "${METAL3REPO}" "${METAL3BRANCH}" \
