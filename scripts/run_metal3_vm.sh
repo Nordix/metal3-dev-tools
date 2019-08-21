9889bb4b582c121c41eac3e8b8b0453cd705843b
maelk <mael.kimmerlin@est.tech>
2019-08-21 12:16:40 +0300

    Add a script to run a metal3 dev env locally from Openstack image
    The script creates a libvirt VM using the Openstack metal3 image
    
    Co-authored-by: Nam Xuan Nguyen <nam.xuan.nguyen@est.tech>

---
+++ scripts/run_metal3_vm.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -uex
+
+echo "Setting up SSH keys"
+
+WORK_DIR=/opt/metal3
+sudo mkdir -p "$WORK_DIR"
+sudo chown -R "${USER}:${USER}" "$WORK_DIR"
+
+mkdir -p ~/.ssh
+if [[ ! -f ~/.ssh/metal3 ]]; then
+  ssh-keygen -q -N "" -f ~/.ssh/metal3
+fi
+cp files/ubuntu-metal3-config.yaml.tpl "$WORK_DIR/ubuntu-metal3-config.yaml"
+sed -i '/    ssh-authorized-keys:/!b;n;c\      - '"$(cat ~/.ssh/metal3.pub)" "$WORK_DIR/ubuntu-metal3-config.yaml"
+
+echo "Downloading Metal3 if needed"
+if [[ ! -f "$WORK_DIR/ubuntu_metal3.qcow2" ]]; then
+  openstack image save --file "$WORK_DIR/ubuntu_metal3.qcow2" airship-ci-ubuntu-metal3-img
+fi
+
+vm_defined=$(virsh list --all | grep ubuntu-metal3 || true )
+echo VM_DEFINED "$vm_defined"
+
+if [[ ! -f "$WORK_DIR/ubuntu_metal3_vm.qcow2" ]] || [[ $vm_defined != *"ubuntu-metal3"* ]] ; then
+  rm "$WORK_DIR/ubuntu_metal3_vm.qcow2" || true
+  cp "$WORK_DIR/ubuntu_metal3.qcow2" "$WORK_DIR/ubuntu_metal3_vm.qcow2"
+  # Resize this image
+  qemu-img resize "$WORK_DIR/ubuntu_metal3_vm.qcow2" 50G
+
+
+  echo "Creating config ISO"
+  # Install this package to generate iso from qcow2 file
+  sudo apt install  -y cloud-utils
+  # Create file iso
+  sudo rm "$WORK_DIR/config.iso" || true
+  cloud-localds "$WORK_DIR/ubuntu-metal3-config.iso" "$WORK_DIR/ubuntu-metal3-config.yaml"
+
+  echo "Creating virtual machine"
+  # Create virtual machine
+  virt-install \
+    --memory 16384 \
+    --vcpus 4 \
+    --cpu host \
+    --name ubuntu-metal3 \
+    --disk "$WORK_DIR/ubuntu_metal3_vm.qcow2,device=disk" \
+    --disk "$WORK_DIR/ubuntu-metal3-config.iso,device=cdrom" \
+    --os-type Linux \
+    --os-variant ubuntu18.04 \
+    --virt-type kvm \
+    --network network=default \
+    --import \
+    --graphics none \
+    --console pty,target_type=serial \
+    --noautoconsole
+
+else
+
+  vm_running=$(virsh list | grep ubuntu-metal3 || true)
+  echo "Starting virtual machine"
+  if  [[ $vm_running != *"ubuntu-metal3"* ]] ; then
+    virsh start centos-metal3
+  fi
+fi
+
+echo "Machine IP : $(sudo virsh net-dhcp-leases default | \
+  grep "airship-ci-ubuntu-metal3-img" | awk '{print $5}' \
+  | cut -d '/' -f1)"
