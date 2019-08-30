0d053070cb99e13f4b678641ac412c4685b72a21
maelk <mael.kimmerlin@est.tech>
2019-08-30 09:51:49 +0300

    Add a wrapper for CI around test integration to comment on GH

---
+++ ci/scripts/tests/integration_test_ci_wrapper.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+
+# Description:
+#   Runs the integration tests for metal3-dev-env
+#   Requires:
+#     - source stackrc file
+#     - openstack ci infra should already be deployed.
+#     - environment variables set:
+#       - AIRSHIP_CI_USER: Ci user for jumphost.
+#       - AIRSHIP_CI_USER_KEY: Path of the CI user private key for jumphost.
+#       - GITHUB_USERNAME: Username for Github API
+#       - GITHUB_PASSWORD: Password for Github API
+#       - BUILD_URL: The URL of the current build
+#       - PR_ID : The id of the pull request
+#       - REPO_ORG: the organization of the repository
+#       - REPO_NAME: the name of the repo
+# Usage:
+#  integration_test_ci_wrapper.sh
+#
+
+CI_DIR="$(dirname "$(readlink -f "${0}")")/../.."
+TESTS_SCRIPTS_DIR="${CI_DIR}/scripts/tests"
+
+curl -H "Authorization: token ${GITHUB_PASSWORD}" -X POST \
+-d "{\"body\": \":revolving_hearts: Test pending : ${BUILD_URL}\", \
+\"event\": \"COMMENT\"}" \
+"https://api.github.com/repos/${REPO_ORG}/${REPO_NAME}/pulls/${PR_ID}/reviews"
+
+"$TESTS_SCRIPTS_DIR/integration_test.sh"
+RETURN_CODE="$?"
+
+if [[ "$RETURN_CODE" == "0" ]]; then
+  RETURN_STATUS=":green_heart: Test Success"
+else
+  RETURN_STATUS=":broken_heart: Test Failure"
+fi
+
+curl -H "Authorization: token ${GITHUB_PASSWORD}" -X POST \
+-d "{\"body\": \"${RETURN_STATUS} : ${BUILD_URL}\", \
+\"event\": \"COMMENT\"}" \
+"https://api.github.com/repos/${REPO_ORG}/${REPO_NAME}/pulls/${PR_ID}/reviews"
+
+exit "${RETURN_CODE}"
