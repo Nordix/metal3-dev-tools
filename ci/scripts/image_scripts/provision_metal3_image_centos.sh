83906384fe1835b9f18809d7ec79fd40c4072e7e
maelk <mael.kimmerlin@est.tech>
2019-11-11 09:07:36 +0200

    Do not bake in metal3 environment

--- ci/scripts/image_scripts/provision_metal3_image_centos.sh
+++ ci/scripts/image_scripts/provision_metal3_image_centos.sh
@@ -27,23 +27,23 @@ sudo mkdir -p /usr/local/bin/
 sudo cp operator-sdk-${OSDK_RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/operator-sdk
 rm operator-sdk-${OSDK_RELEASE_VERSION}-x86_64-linux-gnu
 
-# Install metal3 requirements
-mkdir -p "${M3_DENV_ROOT}"
-if [[ -d "${M3_DENV_PATH}" && "${FORCE_REPO_UPDATE}" == "true" ]]; then
-  sudo rm -rf "${M3_DENV_PATH}"
-fi
-if [ ! -d "${M3_DENV_PATH}" ] ; then
-  pushd "${M3_DENV_ROOT}"
-  git clone "${M3_DENV_URL}"
-  popd
-fi
-pushd "${M3_DENV_PATH}"
-git checkout "${M3_DENV_BRANCH}"
-git pull -r || true
-make install_requirements
-popd
-
-rm -rf "${M3_DENV_PATH}"
+## Install metal3 requirements
+#mkdir -p "${M3_DENV_ROOT}"
+#if [[ -d "${M3_DENV_PATH}" && "${FORCE_REPO_UPDATE}" == "true" ]]; then
+#  sudo rm -rf "${M3_DENV_PATH}"
+#fi
+#if [ ! -d "${M3_DENV_PATH}" ] ; then
+#  pushd "${M3_DENV_ROOT}"
+#  git clone "${M3_DENV_URL}"
+#  popd
+#fi
+#pushd "${M3_DENV_PATH}"
+#git checkout "${M3_DENV_BRANCH}"
+#git pull -r || true
+#make install_requirements
+#popd
+#
+#rm -rf "${M3_DENV_PATH}"
 
 sudo sed -i "0,/.*PermitRootLogin.*/s//PermitRootLogin yes/" /etc/ssh/sshd_config
 
