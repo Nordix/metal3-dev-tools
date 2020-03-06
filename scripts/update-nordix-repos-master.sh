533a9ec68b2f6b38488a277427144311f7ae86c7
fmuyassarov <feruzjon.muyassarov@est.tech>
2020-03-06 15:11:33 +0200

    Add repos to syn with upstream

--- scripts/update-nordix-repos-master.sh
+++ scripts/update-nordix-repos-master.sh
@@ -18,6 +18,8 @@ CAPI_REPO="https://github.com/kubernetes-sigs/cluster-api.git"
 BMO_REPO="https://github.com/metal3-io/baremetal-operator.git"
 M3DOCS_REPO="https://github.com/metal3-io/metal3-docs.git"
 M3DEVENV_REPO="https://github.com/metal3-io/metal3-dev-env.git"
+PROJECTINFRA_REPO="https://github.com/metal3-io/project-infra.git"
+METAL3GITHUBIO_REPO="https://github.com/metal3-io/metal3-io.github.io.git"
 
 NORDIX_CAPIPB_REPO="git@github.com:Nordix/cluster-api-provider-baremetal.git"
 NORDIX_CAPM3_REPO="git@github.com:Nordix/cluster-api-provider-metal3.git"
@@ -25,6 +27,8 @@ NORDIX_CAPI_REPO="git@github.com:Nordix/cluster-api.git"
 NORDIX_BMO_REPO="git@github.com:Nordix/baremetal-operator.git"
 NORDIX_M3DOCS_REPO="git@github.com:Nordix/metal3-docs.git"
 NORDIX_M3DEVENV_REPO="git@github.com:Nordix/metal3-dev-env.git"
+NORDIX_PROJECTINFRA_REPO="git@github.com:Nordix/metal3-project-infra.git"
+NORDIX_METAL3GITHUBIO_REPO="git@github.com:Nordix/metal3-io.github.io.git"
 
 LOCAL_CAPIPB_REPO="${WORKSPACE}/cluster-api-provider-baremetal"
 LOCAL_CAPM3_REPO="${WORKSPACE}/cluster-api-provider-metal3"
@@ -32,14 +36,16 @@ LOCAL_CAPI_REPO="${WORKSPACE}/cluster-api"
 LOCAL_BMO_REPO="${WORKSPACE}/baremetal-operator"
 LOCAL_M3DOCS_REPO="${WORKSPACE}/metal3-docs"
 LOCAL_M3DEVENV_REPO="${WORKSPACE}/metal3-dev-env"
+LOCAL_PROJECTINFRA_REPO="${WORKSPACE}/project-infra"
+LOCAL_METAL3GITHUBIO_REPO="${WORKSPACE}/metal3-io.github.io"
 
 pushd "${SCRIPTPATH}"
 cd ..
 
-UPDATE_REPO="${1:-${LOCAL_CAPIPB_REPO} ${LOCAL_CAPM3_REPO} ${LOCAL_CAPI_REPO} ${LOCAL_BMO_REPO} ${LOCAL_M3DOCS_REPO} ${LOCAL_M3DEVENV_REPO}}"
+UPDATE_REPO="${1:-${LOCAL_CAPIPB_REPO} ${LOCAL_CAPM3_REPO} ${LOCAL_CAPI_REPO} ${LOCAL_BMO_REPO} ${LOCAL_M3DOCS_REPO} ${LOCAL_M3DEVENV_REPO} ${LOCAL_PROJECTINFRA_REPO} ${LOCAL_METAL3GITHUBIO_REPO}}"
 UPDATE_BRANCH="${2:-master}"
-UPSTREAM_REPO="${3:-${CAPIPB_REPO} ${CAPM3_REPO} ${CAPI_REPO} ${BMO_REPO} ${M3DOCS_REPO} ${M3DEVENV_REPO}}"
-NORDIX_REPO="${4:-${NORDIX_CAPIPB_REPO} ${NORDIX_CAPM3_REPO} ${NORDIX_CAPI_REPO} ${NORDIX_BMO_REPO} ${NORDIX_M3DOCS_REPO} ${NORDIX_M3DEVENV_REPO}}"
+UPSTREAM_REPO="${3:-${CAPIPB_REPO} ${CAPM3_REPO} ${CAPI_REPO} ${BMO_REPO} ${M3DOCS_REPO} ${M3DEVENV_REPO} ${PROJECTINFRA_REPO} ${METAL3GITHUBIO_REPO}}"
+NORDIX_REPO="${4:-${NORDIX_CAPIPB_REPO} ${NORDIX_CAPM3_REPO} ${NORDIX_CAPI_REPO} ${NORDIX_BMO_REPO} ${NORDIX_M3DOCS_REPO} ${NORDIX_M3DEVENV_REPO} ${NORDIX_PROJECTINFRA_REPO} ${NORDIX_METAL3GITHUBIO_REPO}}"
 
 # clone upstream repos to jenkins if not found
 i=0
@@ -53,7 +59,7 @@ for index in ${UPDATE_REPO}
 do
     if [ ! -d "${locarray[$i]}" ]; then
       echo "CLONE "${upsarray[$i]}""
-      git clone ${upsarray[$i]}
+      git clone "${upsarray[$i]}" "${locarray[$i]}"
     fi
 i=$(($i+1));
 done
