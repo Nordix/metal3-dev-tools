2700d9bd3e8fd7fb332c58e25fa232bd4e6acef9
Jaakko Kuuskoski <jaakko.kuuskoski@est.tech>
2020-05-25 09:30:22 +0300

    CI test automation script for combined node image and k8s version upgrade

---
+++ upgrade_tests/controlplane_upgrade/cp_osimage_K8S_upgrade.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+source ../common.sh
+
+echo '' > ~/.ssh/known_hosts
+
+provision_controlplane_node
+
+CLUSTER_NAME=$(kubectl get clusters -n metal3 | grep Provisioned | cut -f1 -d' ')
+
+wait_for_ctrlplane_provisioning_start
+
+ORIGINAL_NODE=$(kubectl get bmh -n metal3 | grep control | grep -v ready | cut -f1 -d' ')
+echo "BareMetalHost ${ORIGINAL_NODE} is in provisioning or provisioned state"
+NODE_IP=$(virsh net-dhcp-leases baremetal | grep "${ORIGINAL_NODE}"  | awk '{{print $5}}' | cut -f1 -d\/)
+
+wait_for_ctrlplane_provisioning_complete ${ORIGINAL_NODE} ${NODE_IP} "Original node"
+
+FROM_VERSION=$(kubectl get kcp -n metal3 -oyaml | grep "version: v1" | cut -f2 -d':' | awk '{$1=$1;print}')
+
+if [[ "${FROM_VERSION}" < "${UPGRADED_K8S_VERSION_2}" ]]; then
+  TO_VERSION="${UPGRADED_K8S_VERSION_2}"
+elif [[ "${FROM_VERSION}" > "${KUBERNETES_VERSION}" ]]; then
+  TO_VERSION="${KUBERNETES_VERSION}"
+else
+  exit 0
+fi
+
+M3_MACHINE_TEMPLATE_NAME=$(kubectl get Metal3MachineTemplate -n metal3 -oyaml | grep "name: " | grep controlplane | cut -f2 -d':' | awk '{$1=$1;print}')
+
+Metal3MachineTemplate_OUTPUT_FILE="/tmp/new_image.yaml"
+CLUSTER_UID=$(kubectl get clusters -n metal3 ${CLUSTER_NAME} -o json |jq '.metadata.uid' | cut -f2 -d\") 
+generate_metal3MachineTemplate test1-new-image "${CLUSTER_UID}" "${Metal3MachineTemplate_OUTPUT_FILE}"
+kubectl apply -f "${Metal3MachineTemplate_OUTPUT_FILE}"
+
+echo "Upgrading a control plane node image and k8s version from ${FROM_VERSION} to ${TO_VERSION} in cluster ${CLUSTER_NAME}"
+# replace node image and k8s version in kcp yaml:
+kubectl get kcp -n metal3 -oyaml | sed "s/version: ${FROM_VERSION}/version: ${TO_VERSION}/" | sed "s/name: ${M3_MACHINE_TEMPLATE_NAME}/name: test1-new-image/" | kubectl replace -f -
+
+wait_for_ug_process_to_complete ${ORIGINAL_NODE}
+
+wait_for_orig_node_deprovisioned ${ORIGINAL_NODE}
