08279ac378419dc903176d04d7443ec596901ad3
Jaakko Kuuskoski <jaakko.kuuskoski@est.tech>
2020-06-04 15:50:51 +0300

    Reduced set of upgrade tests for test fw use
    including fixes

--- upgrade_tests/upgrade.sh
+++ upgrade_tests/upgrade.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
+M3_DIR="$(dirname "$(readlink -f "${0}")")/../../.."
+
 # Fetch the upgrade tests from airship-dev-tools
-git clone https://github.com/Nordix/airship-dev-tools.git /tmp/airship-dev-tools-clone
-cp -r /tmp/airship-dev-tools-clone/airship-dev-tools/upgrade_tests "${M3PATH}"/scripts/feature_tests/upgrade
+pushd ${M3_DIR}/scripts/feature_tests/upgrade
+cd /tmp
+git clone https://github.com/Nordix/airship-dev-tools.git
+popd
+
+cp -r /tmp/airship-dev-tools/upgrade_tests ${M3_DIR}/scripts/feature_tests/upgrade
 
 # Run controlplane upgrade tests
-pushd "${M3PATH}/scripts/feature_tests/upgrade/upgrade_tests"
-./controlplane_upgrade/1cp_0w_bootDiskImage_extraNode_upgrade.sh
-./controlplane_upgrade/1cp_0w_k8sBin_extraNode_upgrade.sh
-./controlplane_upgrade/1cp_0w_k8sVer_bootDiskImage_extraNode_upgrade.sh
-./controlplane_upgrade/1cp_0w_k8sVer_extraNode_upgrade.sh
-./controlplane_upgrade/3cp_0w_bootDiskImage_extraNode_upgrade.sh
-./controlplane_upgrade/3cp_0w_k8sVer_extraNode_upgrade.sh
-./controlplane_upgrade/3cp_1w_k8sVer_bootDiskImage_scaleInWorker_upgrade.sh
-./controlplane_upgrade/1cp_1w_kubeadm_update.sh
+pushd "${M3_DIR}/scripts/feature_tests/upgrade/upgrade_tests/controlplane_upgrade"
+#./1cp_0w_bootDiskImage_extraNode_upgrade.sh
+#./1cp_0w_k8sBin_extraNode_upgrade.sh
+#./1cp_0w_k8sVer_bootDiskImage_extraNode_upgrade.sh
+#./1cp_0w_k8sVer_extraNode_upgrade.sh
+#./3cp_0w_bootDiskImage_extraNode_upgrade.sh
+#./3cp_0w_k8sVer_extraNode_upgrade.sh
+./3cp_1w_k8sVer_bootDiskImage_scaleInWorker_upgrade.sh
 popd
 
 # Run cluster level ugprade tests
-pushd "${M3PATH}/scripts/feature_tests/upgrade/upgrade_tests"
+pushd "${M3_DIR}/scripts/feature_tests/upgrade/upgrade_tests"
 ./1cp_1w_bootDiskImage_cluster_upgrade.sh
 popd
 
 # Run worker upgrade cases
-pushd "${M3PATH}/scripts/feature_tests/upgrade/upgrade_tests"
-./workers_upgrade/1cp_1w_bootDiskImage_extraNode_upgrade.sh
-./workers_upgrade/1cp_1w_bootDiskImage_scaleOutWorkers_upgrade.sh
-./workers_upgrade/1cp_3w_bootDiskImage_scaleInWorkers_upgrade_both.sh
-./workers_upgrade/1cp_3w_bootDiskImage_scaleInWorkers_upgrade.sh
+pushd "${M3_DIR}/scripts/feature_tests/upgrade/upgrade_tests/workers_upgrade"
+#./1cp_1w_bootDiskImage_extraNode_upgrade.sh
+#./1cp_1w_bootDiskImage_scaleOutWorkers_upgrade.sh
+./1cp_3w_bootDiskImage_scaleInWorkers_upgrade_both.sh
+#./1cp_3w_bootDiskImage_scaleInWorkers_upgrade.sh
 popd
 
 # clean up the copied directories
-rm -rf /tmp/airship-dev-tools-clone
-pushd "${M3PATH}/scripts/feature_tests/upgrade"
+rm -rf /tmp/airship-dev-tools
+pushd "${M3_DIR}/scripts/feature_tests/upgrade"
 rm -rf upgrade_tests
 popd
