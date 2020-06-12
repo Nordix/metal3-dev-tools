3f9653d745baf44e60b742ea9e00199e12d04601
smoshiur1237 <moshiur.rahman@est.tech>
2020-06-12 08:56:06 +0300

    Fix loop issue and update code for the priority use case.

--- upgrade_tests/workers_upgrade/1cp_3w_bootDiskImage_scaleInWorkers_upgrade_both.sh
+++ upgrade_tests/workers_upgrade/1cp_3w_bootDiskImage_scaleInWorkers_upgrade_both.sh
@@ -15,7 +15,7 @@ export new_cp_metal3MachineTemplate_name="test1-new-controlplane-image"
 # TODO: cleanup
 set_number_of_node_replicas 1
 set_number_of_master_node_replicas 1
-set_number_of_worker_node_replicas 3
+set_number_of_worker_node_replicas 1
 
 # provision a controlplane node
 provision_controlplane_node
@@ -25,33 +25,24 @@ CP_NODE=$(kubectl get bmh -n metal3 | grep control | grep -v ready | cut -f1 -d'
 echo "BareMetalHost ${CP_NODE} is in provisioning or provisioned state"
 CP_NODE_IP=$(sudo virsh net-dhcp-leases baremetal | grep "${CP_NODE}"  | awk '{{print $5}}' | cut -f1 -d\/)
 wait_for_ctrlplane_provisioning_complete ${CP_NODE} ${CP_NODE_IP} "original controlplane node"
+
 # apply CNI
 ssh -o PasswordAuthentication=no -o "StrictHostKeyChecking no" "${UPGRADE_USER}@${CP_NODE_IP}" -- kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
-set_number_of_node_replicas 3
-
 provision_worker_node
-# Wait until the worker joins AND the state is ready
-# Workers' upgrade requires CNI and kubernetes nodes' status should be ready.
-echo "Waiting for worker to join the cluster"
-for i in {1..3600};do
-  r_count=$(ssh -o PasswordAuthentication=no -o "StrictHostKeyChecking no" "${UPGRADE_USER}@${CP_NODE_IP}" -- kubectl get nodes | grep Ready | wc -l)
-  if [[ "$r_count" == '2' ]]; then
-    echo "The worker has joined and all kubernetes nodes are in Ready state"
-    break
-  fi
-  echo -n "*"
-  sleep 5
-  if [[ "${i}" -ge 3600 ]]; then
-	  echo "Error: It took too long for a worker to join the cluster"
-	  exit 1
-  fi
-done
+wait_for_worker_provisioning_start
+W_NODE=$(kubectl get bmh -n metal3 | grep worker | grep -v ready | cut -f1 -d' ')
+W_NODE_IP=$(sudo virsh net-dhcp-leases baremetal | grep "${W_NODE}"  | awk '{{print $5}}' | cut -f1 -d\/)
+wait_for_worker_provisioning_complete 2 ${W_NODE} ${W_NODE_IP} "Worker node"
 
-wait_for_worker_to_scale_out ${CP_NODE_IP}
+# TODO: cleanup
+set_number_of_node_replicas 3
+set_number_of_worker_node_replicas 3
+wait_for_worker_to_scale_to 3 ${CP_NODE_IP}
 
 echo "Create a new metal3MachineTemplate with new node image for worker nodes"
 wr_Metal3MachineTemplate_OUTPUT_FILE="/tmp/wr_new_image.yaml"
+
 CLUSTER_UID=$(kubectl get clusters -n metal3 test1 -o json |jq '.metadata.uid' | cut -f2 -d\")
 IMG_CHKSUM=$(curl -s https://cloud-images.ubuntu.com/bionic/current/MD5SUMS | grep bionic-server-cloudimg-amd64.img | cut -f1 -d' ')
 generate_metal3MachineTemplate "${new_wr_metal3MachineTemplate_name}" "${CLUSTER_UID}" "${wr_Metal3MachineTemplate_OUTPUT_FILE}" "${IMG_CHKSUM}"
@@ -62,39 +53,41 @@ kubectl apply -f "${wr_Metal3MachineTemplate_OUTPUT_FILE}"
 kubectl get machinedeployment -n metal3 test1 -o json | jq '.spec.strategy.rollingUpdate.maxSurge=1|.spec.strategy.rollingUpdate.maxUnavailable=1' | kubectl apply -f-
 kubectl get machinedeployment -n metal3 test1 -o json | jq '.spec.template.spec.infrastructureRef.name="test1-new-workers-image"' | kubectl apply -f-
 
-echo "Waiting for start of node image upgrade of all worker nodes"
+echo "Node image upgrade started for ${NUM_OF_WORKER_REPLICAS} worker nodes"
 for i in {1..3600};do
   count=$(kubectl get bmh -n metal3 | awk 'NR>1'| grep -i ${new_wr_metal3MachineTemplate_name} | wc -l)
-  if [ $count -lt 3 ]; then
-      echo -n "-"
-	  if [[ "${i}" -ge 3600 ]];then
-		  echo "Error: Upgrade on some or all worker nodes did not start in time"
-		  exit 1
+  if [[ "${count}" -lt "${NUM_OF_WORKER_REPLICAS}" ]]; then
+      echo -n "-"	  
+      if [[ "${i}" -ge 3600 ]];then
+        echo "Error: Upgrade on some or all worker nodes did not start in time"
+        exit 1
       fi
       sleep 5
-	  continue
+      continue
   else
-    echo "Upgrade of all worker nodes has started"
-	  break
+      echo "Upgrade of all worker nodes has finished"
+      break
   fi
 done
 
 # Verify that all upgraded worker nodes have joined the cluster.
-echo "Waiting for all upgraded worker nodes to join the cluster"
+echo "Waiting for ${NUM_OF_WORKER_REPLICAS} upgraded worker nodes to join the cluster"
 for i in {1..3600};do
-  echo -n "*"
-  replicas=$(ssh "-o LogLevel=quiet" -o "UserKnownHostsFile=/dev/null" -o PasswordAuthentication=no -o "StrictHostKeyChecking no" "${UPGRADE_USER}@${CP_NODE_IP}" -- kubectl get nodes | awk 'NR>1'| grep -v master | wc -l)
-  if [[ "$replicas" == "3" ]]; then
-    echo ''
-    echo "Upgraded worker nodes have joined the cluster"
-    break
-  fi
-  sleep 5
-  if [[ "${i}" -ge 3600 ]];then
-		  echo "Error: Upgraded worker node did not join the cluster in time"
-		  exit 1
-  fi
+    echo -n "*"
+    replicas=$(ssh "-o LogLevel=quiet" -o "UserKnownHostsFile=/dev/null" -o PasswordAuthentication=no -o "StrictHostKeyChecking no" "${UPGRADE_USER}@${CP_NODE_IP}" -- kubectl get nodes| grep -v master | wc -l)
+    if [[ "${replicas}" == "${NUM_OF_WORKER_REPLICAS}" ]]; then
+        echo ''
+        echo "Upgraded worker nodes have joined the cluster"
+        break
+    fi
+    sleep 5
+    if [[ "${i}" -ge 3600 ]];then
+          echo "Error: Upgraded worker node did not join the cluster in time"
+          exit 1
+    fi
 done
+echo "Successfully upgraded multiple workers"
+
 # deploy workloads on workers
 cat <<EOF | kubectl apply -f -
 apiVersion: apps/v1
@@ -117,8 +110,6 @@ spec:
 EOF
 
 # wait for workloads to be ready
-
-
 echo "Waiting for workloads to be ready"
 for i in {1..1800};do
     #workload_replicas=$(ssh "-o LogLevel=quiet" -o "UserKnownHostsFile=/dev/null" -o PasswordAuthentication=no -o "StrictHostKeyChecking no" "${UPGRADE_USER}@${CP_NODE_IP}" -- kubectl get deployments workload-1-deployment -o json | jq '.status.readyReplicas')
@@ -138,27 +129,14 @@ done
 
 # scale in workers, change replicas to two
 echo "Scaling in worker nodes"
-kubectl get machinedeployment -n metal3 test1 -o json | jq '.spec.replicas=2' | kubectl apply -f-
-for i in {1..1800};do
-    replicas=$(ssh "-o LogLevel=quiet" -o "UserKnownHostsFile=/dev/null" -o PasswordAuthentication=no -o "StrictHostKeyChecking no" "${UPGRADE_USER}@${CP_NODE_IP}" -- kubectl get nodes | awk 'NR>1' | grep -v master | wc -l)
-    if [[ "$replicas" == "2" ]]; then
-        echo ''
-        echo "Successfully scaled in worker nodes"
-        break
-    fi
-    echo -n "*"
-    sleep 5
-    if [[ "${i}" -ge 1800 ]];then
-        echo "Error: Scaling in of workers nodes took too long"
-        exit 1
-    fi
-done
+
+# TODO: cleanup
+set_number_of_node_replicas 2
+set_number_of_worker_node_replicas 2
+wait_for_worker_to_scale_to 2 ${CP_NODE_IP}
 
 # TBD
-# upgrade a CP
-#   ****** change os reference
-#   ****** repeate the waiting and verification as well.
-
+# upgrade a Controlplane
 echo "Create a new metal3MachineTemplate with new node image for both controlplane node"
 cp_Metal3MachineTemplate_OUTPUT_FILE="/tmp/cp_new_image.yaml"
 CLUSTER_UID=$(kubectl get clusters -n metal3 test1 -o json |jq '.metadata.uid' | cut -f2 -d\")
@@ -186,27 +164,13 @@ for i in {1..3600};do
   NEW_CP_NODE_IP=$(sudo virsh net-dhcp-leases baremetal | grep "${NEW_CP_NODE}"  | awk '{{print $5}}' | cut -f1 -d\/)
   break # if we made it here, then the controlplane node is being upgraded.
 done
-wait_for_ctrlplane_provisioning_complete ${NEW_CP_NODE} ${NEW_CP_NODE_IP} "upgraded controlplane node"
 
 # scale out workers, change repliacs back three
 echo "Scaling out worker nodes"
-kubectl get machinedeployment -n metal3 test1 -o json | jq '.spec.replicas=3' | kubectl apply -f-
-for i in {1..1800};do
-    replicas=$(ssh "-o LogLevel=quiet" -o "UserKnownHostsFile=/dev/null" -o PasswordAuthentication=no -o "StrictHostKeyChecking no" "${UPGRADE_USER}@${CP_NODE_IP}" -- kubectl get nodes | awk 'NR>1' | grep -v master | wc -l)
-    if [[ "$replicas" == "3" ]]; then
-        echo ''
-        echo "Successfully scaled out worker nodes"
-        break
-    fi
-    echo -n "*"
-    sleep 5
-    if [[ "${i}" -ge 1800 ]];then
-        echo "Error: Scaling out of workers nodes took too long"
-        exit 1
-    fi
-done
-
-echo "Upgrading of (1M + 3W) using scaling in of workers has succeeded"
+set_number_of_node_replicas 3
+set_number_of_worker_node_replicas 3
+wait_for_worker_to_scale_to 3 ${CP_NODE_IP}
+echo "Upgrading of both (1M + 3W) using scaling in of workers has succeeded"
 
 deprovision_cluster
 wait_for_cluster_deprovisioned
