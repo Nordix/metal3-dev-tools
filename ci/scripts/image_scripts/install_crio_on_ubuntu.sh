a3a59a34cf58076b9c04c11d8f205f561f8b2a1c
Feruzjon Muyassarov <feruzjon.muyassarov@est.tech>
2021-01-07 11:27:23 +0200

    Use Kubernetes latest release 1.20
    Switch to k8s 1.20 release and to CRI-O as container runtime for kubelet

---
+++ ci/scripts/image_scripts/install_crio_on_ubuntu.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -uex
+
+export CRICTL_VERSION=${CRICTL_VERSION:-"v1.20.0"}
+OS=${OS:-"xUbuntu_20.04"}
+# CRI-O version goes 1:1 with Kubernetes version. Thus,
+# please make sure that k8s version given in
+# KUBERNETES_VERSION variable matches CRI-O version
+# give in VERSION variable.
+VERSION=${VERSION:-"1.20"}
+
+# Prerequisites for CRI-O
+# Create the .conf file to load the modules at bootup
+cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Set up required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sudo sysctl --system
+
+# Install CRI-O
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
+EOF
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
+EOF
+
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers-cri-o.gpg add -
+
+sudo apt-get update
+sudo apt-get install cri-o cri-o-runc -y
+
+# Start CRI-O
+sudo systemctl daemon-reload
+sudo systemctl start crio
+
+# Download crictl
+curl -LO https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-amd64.tar.gz
+sudo tar -C /usr/local/bin -xzf crictl-${CRICTL_VERSION}-linux-amd64.tar.gz
