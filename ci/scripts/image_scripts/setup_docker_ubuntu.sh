ad329e1039c9c66697391080b61004b8e088013a
Anwar Hassen <anwar.hassen@est.tech>
2021-04-13 11:28:12 +0300

    Add ubuntu to user to docker group

--- ci/scripts/image_scripts/setup_docker_ubuntu.sh
+++ ci/scripts/image_scripts/setup_docker_ubuntu.sh
@@ -17,5 +17,6 @@ sudo apt-get update
 sudo apt-get install -y docker-ce=5:19.03.14~3-0~ubuntu-focal docker-ce-cli=5:19.03.14~3-0~ubuntu-focal containerd.io jq
 sudo groupadd docker
 sudo usermod -aG docker "${USER}"
+sudo usermod -aG docker ubuntu
 sudo systemctl enable docker
 sudo systemctl restart docker
