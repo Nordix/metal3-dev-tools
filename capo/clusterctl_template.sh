fe24a13412b952ee5720c680fb21a2caa528924d
Anwar Hassen <anwar.hassen@est.tech>
2021-06-14 11:57:09 +0300

    Make the capo e2e component configure its own environment
    Signed-off-by: Anwar Hassen <anwar.hassen@est.tech>

--- capo/clusterctl_template.sh
+++ capo/clusterctl_template.sh
@@ -15,12 +15,16 @@ CAPO_ENVRC="${CAPO_REPO}/templates/env.rc"
 make -C "$CAPI_REPO" clusterctl
 
 source "$CAPO_ENVRC" /tmp/clouds.yaml "${OS_CLOUD_NAME}"
-env | grep OPENSTACK_CLOUD | sed 's/^/export /' > /tmp/e2e_vars_openstack.sh
+env | grep OPENSTACK_CLOUD | sed 's/^/export /' > /tmp/capo_vars_openstack.sh
 
 source ./capo_os_vars.rc
+source /tmp/capo_vars_openstack.sh
 
 "$CLUSTERCTL" config cluster "$CLUSTER_NAME" \
     --kubernetes-version v1.19.1 \
     --from "${CAPO_REPO}/templates/cluster-template-without-lb.yaml" \
     --control-plane-machine-count=1 \
     --worker-machine-count=1 > /tmp/$CLUSTER_NAME.yaml
+
+# Required to run in our openstack instances
+sed -i 's/cloud-provider: openstack/cloud-provider: external/' /tmp/$CLUSTER_NAME.yaml
