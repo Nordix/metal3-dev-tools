84f26887979830c7d8377515424d12414a7e7f7b
kashifest <kashif.khan@est.tech>
2021-08-24 15:01:28 +0300

    List prepulled container images for verification

--- ci/scripts/image_scripts/provision_node_image_centos.sh
+++ ci/scripts/image_scripts/provision_node_image_centos.sh
@@ -49,6 +49,8 @@ sudo rm "${HOME}"/.ssh/authorized_keys
 # Download container images
 "${SCRIPTS_DIR}"/target_cluster_container_images.sh
 
+sudo "${CONTAINER_RUNTIME}" images
+
 # Reset cloud-init to run on next boot.
 "${SCRIPTS_DIR}"/reset_cloud_init.sh
 
