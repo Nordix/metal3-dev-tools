8e188bd382c7f54033bc9119f0e0d9646175aba2
Rozzii <adam.rozman@est.tech>
2021-08-25 15:02:08 +0300

    Add basic bootstrap test to IPA build process
    This test simply checks whether the metal3-dev-env could
    be bootstrapped by utilizing the newly built IPA archive.
    
    In case the "bootstrap test" fails the IPA archive won't
    be uploaded to Nordix artifactory.

--- ci/scripts/image_scripts/build_ipa.sh
+++ ci/scripts/image_scripts/build_ipa.sh
@@ -20,6 +20,11 @@ IPA_BASE_OS="${IPA_BASE_OS:-centos}"
 IPA_BASE_OS_RELEASE="${IPA_BASE_OS_RELEASE:-8-stream}"
 OPENSTACK_REQUIREMENTS_REF="${OPENSTACK_REQUIREMENTS_REF:-master}"
 IRONIC_SIZE_LIMIT_MB=500
+ENABLE_BOOTSTRAP_TEST="${ENABLE_BOOTSTRAP_TEST:-true}"
+DEV_ENV_REPO_LOCATION="${DEV_ENV_REPO_LOCATION:-/tmp/dib/metal3-dev-env}"
+IMAGE_REGISTRY="registry.nordix.org"
+CONTAINER_IMAGE_REPO="airship"
+STAGING="${STAGING:-false}"
 # Install required packages
 sudo apt install --yes python3-pip python3-virtualenv qemu-utils
 
@@ -34,11 +39,11 @@ git clone --single-branch --branch "${IPA_BUILDER_BRANCH}" "${IPA_BUILDER_REPO}"
 git clone --single-branch --branch "${IPA_REF}" "${IPA_REPO}"
 
 # Generate the IPA image identifier string
-cd "ironic-python-agent"
+pushd "./ironic-python-agent"
 # IDENTIFIER is the git commit of the HEAD and the ISO 8061 UTC timestamp
 IPA_IDENTIFIER="$(date --utc +"%Y%m%dT%H%MZ")-$(git rev-parse --short HEAD)"
 echo "IPA_IDENTIFIER is the following:${IPA_IDENTIFIER}"
-cd ..
+popd
 
 # Install the cloned IPA builder tool
 virtualenv venv
@@ -59,6 +64,9 @@ ironic-python-agent-builder --output "${IPA_IMAGE_NAME}" \
     --element='devuser' --element='openssh-server' \
     --element='extra-hardware' --verbose
 
+# Deactivate the python virtual environment
+deactivate
+
 # Package the initramfs and kernel images to a tar archive
 tar --create --verbose --file="${IPA_IMAGE_TAR}" \
     "./${IPA_IMAGE_NAME}.kernel" \
@@ -73,11 +81,29 @@ if [ ${filesize_MB} -ge ${IRONIC_SIZE_LIMIT_MB} ]; then
     exit 1
 fi
 
+# Test whether the newly built IPA is compatible with the choosen Ironic version and with
+# the Metal3 dev-env
+if $ENABLE_BOOTSTRAP_TEST; then
+    git clone --single-branch --branch "master" "https://github.com/metal3-io/metal3-dev-env"
+    source "/tmp/var.sh"
+    export IRONIC_IMAGE="${IMAGE_REGISTRY}/${CONTAINER_IMAGE_REPO}/ironic-image:${IRONIC_TAG}"
+    export USE_LOCAL_IPA=true
+    export IPA_DOWNLOAD_ENABLED=false
+    pushd "${DEV_ENV_REPO_LOCATION}"
+    make test
+    popd
+fi
+
+REVIEW_ARTIFACTORY_PATH="airship/images/ipa/review/${IPA_BASE_OS}/${IPA_BASE_OS_RELEASE}/${IPA_IDENTIFIER}/${IPA_IMAGE_TAR}"
+STAGING_ARTIFACTORY_PATH="airship/images/ipa/staging/${IPA_BASE_OS}/${IPA_BASE_OS_RELEASE}/${IPA_IDENTIFIER}/${IPA_IMAGE_TAR}"
 # Upload the newly built image
 if ! $DISABLE_UPLOAD ; then
     # shellcheck source=/dev/null
     source "${RT_UTILS}"
-    DST_PATH="airship/images/ipa/centos/${IPA_IDENTIFIER}/${IPA_IMAGE_TAR}"
-    rt_upload_artifact  "${IPA_IMAGE_TAR}" "${DST_PATH}" "0"
+    if $STAGING; then
+        rt_upload_artifact  "${IPA_IMAGE_TAR}" "${STAGING_ARTIFACTORY_PATH}" "0"
+    else
+        rt_upload_artifact  "${IPA_IMAGE_TAR}" "${REVIEW_ARTIFACTORY_PATH}" "0"
+    fi
 fi
 
