a46c5527a3fff9ba4b1754e28fa1c8681ad01084
Feruzjon Muyassarov <feruzjon.muyassarov@est.tech>
2021-09-23 15:35:15 +0300

    Switch kind version to 1.21.2 and crictl to 1.21.0

--- ci/scripts/image_scripts/source_cluster_container_images.sh
+++ ci/scripts/image_scripts/source_cluster_container_images.sh
@@ -10,7 +10,7 @@ export IMG_CENTOS_IMG="${IMG_CENTOS_IMG:-"docker.io/centos:centos8"}"
 
 if [[ "${IMAGE_OS}" == "Ubuntu" ]]; then
     export KUBERNETES_VERSION=${KUBERNETES_VERSION:-"v1.21.2"}
-    export KIND_NODE_IMAGE_VERSION=${KIND_NODE_IMAGE_VERSION:-"v1.22.0"}
+    export KIND_NODE_IMAGE_VERSION=${KIND_NODE_IMAGE_VERSION:-"v1.21.2"}
     # Since capm3 v1a4 tests can only survive with k8s < v1.22
     # the following docker image also needs to be a part of the disk image.
     export IMG_KIND_NODE_IMAGE="${IMG_KIND_NODE_IMAGE:-"kindest/node:${KIND_NODE_IMAGE_VERSION}"}"
