a8e328d31607142b49763dd7a85e151eddb252d6
Rozzii <adam.rozman@est.tech>
2021-10-04 15:12:24 +0300

    Fix issues related to ipa pipeline improvements
    This change adds the following fixes:
      - Fixes missing metal3-dev-env repository commit variable
      - Fixes missing ironic-inspector repository commit variable
      - Adds default refspecs for ironic related repositories
      - Organizes the IPA '.tar' archive's contet to a single directory
        inside the archive
      - Fix shellcheck warnings
      - Fix incorrect commit hash length used for IPA archive repository
        name

--- ci/scripts/image_scripts/run_build_ironic.sh
+++ ci/scripts/image_scripts/run_build_ironic.sh
@@ -9,8 +9,8 @@ sudo apt update -y
 # Example: refs/changes/74/804074/1
 IMAGE_REGISTRY="registry.nordix.org"
 CONTAINER_IMAGE_REPO="airship"
-IRONIC_REFSPEC="${IRONIC_REFSPEC:-}"
-IRONIC_INSPECTOR_REFSPEC="${IRONIC_INSPECTOR_REFSPEC:-}"
+IRONIC_REFSPEC="${IRONIC_REFSPEC:-+refs/heads/master:refs/remotes/origin/master}"
+IRONIC_INSPECTOR_REFSPEC="${IRONIC_INSPECTOR_REFSPEC:-+refs/heads/master:refs/remotes/origin/master}"
 IRONIC_INSPECTOR_REPO="https://opendev.org/openstack/ironic-inspector.git"
 IRONIC_REPO="https://opendev.org/openstack/ironic.git"
 IRONIC_IMAGE_REPO="https://github.com/metal3-io/ironic-image.git"
@@ -19,12 +19,20 @@ IRONIC_IMAGE_BRANCH="${IRONIC_IMAGE_BRANCH:-"master"}"
 PATCH_LIST_FILE=patchList.txt
 
 # Login to the Nordix container registry
-docker login "${IMAGE_REGISTRY}" -u $DOCKER_USER -p $DOCKER_PASSWORD
+docker login "${IMAGE_REGISTRY}" -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
+
+# Gather ironic-inspector commit hash
+git clone "${IRONIC_INSPECTOR_REPO}"
+pushd "ironic-inspector"
+git fetch origin "${IRONIC_INSPECTOR_REFSPEC}"
+git checkout FETCH_HEAD
+IRONIC_INSPECTOR_COMMIT="$(git rev-parse HEAD)"
+popd
 
 # Create a unique tag for Ironic image
 git clone ${IRONIC_REPO}
 pushd ironic
-git fetch "https://review.opendev.org/openstack/ironic" ${IRONIC_REFSPEC} && git checkout FETCH_HEAD
+git fetch "https://review.opendev.org/openstack/ironic" "${IRONIC_REFSPEC}" && git checkout "FETCH_HEAD"
 IRONIC_TAG="${IRONIC_TAG:-"$(git rev-parse --short HEAD)"}"
 IRONIC_COMMIT="$(git rev-parse HEAD)"
 echo "IRONIC_TAG is: ${IRONIC_TAG}"
@@ -50,14 +58,14 @@ else
     IRONIC_IMAGE_REPO_COMMIT="$(git rev-parse HEAD)"
 
     # Create a patchlist
-    touch ${PATCH_LIST_FILE}
-    if [ -z $IRONIC_REFSPEC ]; then
+    touch "${PATCH_LIST_FILE}"
+    if [ -z "${IRONIC_REFSPEC}" ]; then
       echo "No Ironic refspec is provided to checkout. Going to build the image from master"
       docker build -t "${IMAGE_REGISTRY}/${CONTAINER_IMAGE_REPO}/ironic-image:${IRONIC_TAG}" .
     else
       echo "Ironic patch we are going to switch to is: ${IRONIC_REFSPEC}"
       PROJECT_DIR="openstack/ironic"
-      cat << EOF | tee -a ${PATCH_LIST_FILE}
+      cat << EOF | tee -a "${PATCH_LIST_FILE}"
 ${PROJECT_DIR} ${IRONIC_REFSPEC}
 ${PROJECT_DIR}-inspector ${IRONIC_INSPECTOR_REFSPEC}
 EOF
@@ -75,12 +83,13 @@ docker logout "${IMAGE_REGISTRY}"
 # Create Ironic metadata and save it to file
 touch /tmp/metadata.txt
 cat << EOF > /tmp/metadata.txt
-IRONIC_REFSPEC="${IRONIC_REFSPEC}"
 IRONIC_REPO="${IRONIC_REPO}"
+IRONIC_REFSPEC="${IRONIC_REFSPEC}"
 IRONIC_COMMIT="${IRONIC_COMMIT}"
 IRONIC_IMAGE_REPO="${IRONIC_IMAGE_REPO}"
 IRONIC_IMAGE_REPO_COMMIT="${IRONIC_IMAGE_REPO_COMMIT}"
 IRONIC_IMAGE_BRANCH="${IRONIC_IMAGE_BRANCH}"
 IRONIC_INSPECTOR_REFSPEC="${IRONIC_INSPECTOR_REFSPEC}"
 IRONIC_INSPECTOR_REPO="${IRONIC_INSPECTOR_REPO}"
+IRONIC_INSPECTOR_COMMIT="${IRONIC_INSPECTOR_COMMIT}"
 EOF
