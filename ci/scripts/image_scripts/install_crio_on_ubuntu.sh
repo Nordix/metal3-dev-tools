1a726922bdb4177334cbb6291a526c0d8fde6eb5
kashifest <kashif.khan@est.tech>
2022-05-19 11:03:24 +0300

    Fix Ubuntu version in node image names

--- ci/scripts/image_scripts/install_crio_on_ubuntu.sh
+++ ci/scripts/image_scripts/install_crio_on_ubuntu.sh
@@ -3,7 +3,7 @@
 set -uex
 
 export CRICTL_VERSION=${CRICTL_VERSION:-"v1.22.0"}
-OS=${OS:-"xUbuntu_20.04"}
+OS=${OS:-"xUbuntu_22.04"}
 # CRI-O version goes 1:1 with Kubernetes version. Thus,
 # please make sure that k8s version given in
 # KUBERNETES_VERSION variable matches CRI-O version
