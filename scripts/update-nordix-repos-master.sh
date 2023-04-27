4da7665ad4fd5aee329001145b9140b697768cc0
Kashif Khan <kashif.khan@est.tech>
2023-04-27 14:40:33 +0300

    Add release-1.4 branch updates

--- scripts/update-nordix-repos-master.sh
+++ scripts/update-nordix-repos-master.sh
@@ -150,10 +150,10 @@ done
 
 # Sync non default branches on selected repos
 
-CAPM3_RELEASE_BRANCHES="release-0.5 release-1.1 release-1.2 release-1.3"
+CAPM3_RELEASE_BRANCHES="release-0.5 release-1.1 release-1.2 release-1.3 release-1.4"
 update_custom_branch "$CAPM3_RELEASE_BRANCHES" "$LOCAL_CAPM3_REPO" "$NORDIX_CAPM3_REPO"
 
-IPAM_RELEASE_BRANCHES="release-0.1 release-1.1 release-1.2 release-1.3"
+IPAM_RELEASE_BRANCHES="release-0.1 release-1.1 release-1.2 release-1.3 release-1.4"
 update_custom_branch "$IPAM_RELEASE_BRANCHES" "$LOCAL_IPAM_REPO" "$NORDIX_IPAM_REPO"
 
 CAPI_RELEASE_BRANCHES="release-0.4 release-1.1 release-1.2 release-1.3 release-1.4"
