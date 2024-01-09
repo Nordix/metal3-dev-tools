43d2ef120c82c68533bf6fdcab77641f352da3c3
Kashif Khan <kashif.khan@est.tech>
2024-01-09 09:27:39 +0200

    Bump k8s to v1.29.0
    Signed-off-by: Kashif Khan <kashif.khan@est.tech>

--- ci/scripts/image_scripts/install_crio_on_ubuntu.sh
+++ ci/scripts/image_scripts/install_crio_on_ubuntu.sh
@@ -2,8 +2,8 @@
 
 set -uex
 
-export CRICTL_VERSION=${CRICTL_VERSION:-"v1.28.0"}
-export CRIO_VERSION=${CRIO_VERSION:-"v1.28.2"}
+export CRICTL_VERSION=${CRICTL_VERSION:-"v1.29.0"}
+export CRIO_VERSION=${CRIO_VERSION:-"v1.29.0"}
 
 # Prerequisites for CRI-O
 # Create the .conf file to load the modules at bootup
@@ -25,7 +25,7 @@ EOF
 sudo sysctl --system
 
 # Install CRI-O
-curl https://raw.githubusercontent.com/cri-o/cri-o/"${CRIO_VERSION}"/scripts/get | sudo bash -s -- -t "${CRIO_VERSION}"
+curl https://raw.githubusercontent.com/cri-o/packaging/main/get | sudo bash -s -- -t "${CRIO_VERSION}"
 # Start CRI-O
 sudo systemctl daemon-reload
 sudo systemctl start crio
