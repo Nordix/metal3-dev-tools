b82b27044ab30e4a41526d79d96477f85675fd4b
Tero Kauppinen <tero.kauppinen@est.tech>
2025-09-23 15:04:30 +0300

    Instead of fetching users' public keys from artifactory, store public keys in the repository for better keeping track of registered users.
    Remove support and options for artifactory. Some legacy
    scripts are still using artifactory utilities.
    
    Signed-off-by: Tero Kauppinen <tero.kauppinen@est.tech>

--- ci/scripts/README.md
+++ ci/scripts/README.md
@@ -4,9 +4,10 @@ Author: [Tero Kauppinen](mailto:tero.kauppinen@est.tech)
 
 ## Sub-directories
 
-- [artifactory](./artifactory/): User information management in the artifactory
+- [artifactory](./artifactory/): Artifactory utilities
 - [common](./common/): Common helper functions such as option management
 - [harbor](./harbor/): Legacy Harbor API
 - [image_scripts](./image_scripts/): Legacy image management
 - [jumphost](./jumphost/): Jumphost management
 - [openstack](./openstack/): Infrastructure management for OpenStack
+- [public_keys](./public_keys/): Public keys for the jumphost users
--- ci/scripts/artifactory/utils.sh
+++ ci/scripts/artifactory/utils.sh
@@ -6,10 +6,6 @@ COMMON_SCRIPTS_DIR="${CI_DIR}/scripts/common"
 # shellcheck source=ci/scripts/common/utils.sh
 . "${COMMON_SCRIPTS_DIR}/utils.sh"
 
-# Artifactory configuration
-RT_METAL3_DIR="metal3"
-RT_USERS_DIR="${RT_METAL3_DIR}/users"
-
 # ================ Generic Artifactory Helper Functions ===============
 
 # Description:
@@ -39,58 +35,6 @@ rt_upload_artifact() {
 
   common_verbose "Upload artifact: ${SRC_PATH},${DST_PATH},${ANONYMOUS}"
   common_run -- curl "${ARGS[@]}" "${RT_URL}/${DST_PATH}"
-}
-
-# Description:
-# Download artifact from artifactory.
-#
-# Anonymous flag is set to perform all operations anonymously.
-# If anonymous flag is not set then credentials should be set
-# in the following environment variables.
-#   RT_USER: artifactory user name.
-#   RT_TOKEN: artifactory password or api token
-#
-# Usage:
-#   rt_download_artifact <src_file_path> <dest_file_path> <anonymous:0/1>
-#
-rt_download_artifact() {
-  SRC_PATH="${1:?}"
-  DST_PATH="${2:?}"
-  ANONYMOUS="${3:-1}"
-
-  common_verbose "Download artifact: ${SRC_PATH},${DST_PATH},${ANONYMOUS}"
-  _CMD="curl -s \
-    $( ([[ "${ANONYMOUS}" != 1 ]] && echo " -u${RT_USER:?}:${RT_TOKEN:?}") || true) \
-    -XGET \
-    ${RT_URL}/${SRC_PATH} \
-    -o ${DST_PATH}"
-
-  eval "${_CMD}"
-}
-
-# Description:
-# Download artifact from artifactory and dumps it on stdout.
-#
-# Anonymous flag is set to perform all operations anonymously.
-# If anonymous flag is not set then credentials should be set
-# in the following environment variables.
-#   RT_USER: artifactory user name.
-#   RT_TOKEN: artifactory password or api token
-#
-# Usage:
-#   rt_cat_artifact <src_file_path> <anonymous:0/1>
-#
-rt_cat_artifact() {
-  SRC_PATH="${1:?}"
-  ANONYMOUS="${2:-1}"
-
-  common_verbose "Cat artifact: ${SRC_PATH},${ANONYMOUS}"
-  _CMD="curl -s \
-    $( ([[ "${ANONYMOUS}" != 1 ]] && echo " -u${RT_USER:?}:${RT_TOKEN:?}") || true) \
-    -XGET \
-    ${RT_URL}/${SRC_PATH}"
-
-  eval "${_CMD}"
 }
 
 # Description:
@@ -195,95 +139,4 @@ rt_delete_multiple_artifacts() {
       echo "INFO:${DIR_TO_CLEAN}/${item} has been deleted!"
     fi
   done
-}
-
-# ================ Users Artifactory Helper Functions ===============
-
-# Description:
-# Gets all the keys for a user from metal3/users/<username>
-# directory. If `keyname` is set 1, also the keyname will
-# be included in the output.
-#
-# Usage:
-#   rt_get_user_public_keys <username> <keyname:0/1>
-#
-rt_get_user_public_keys() {
-
-  USER="${1:?}"
-  KEYNAME="${2:-0}"
-  USER_KEY_FILES="$(rt_list_directory "${RT_USERS_DIR}/${USER}" \
-    | jq -r '.children[]? |select(.folder==false) |.uri')" > /dev/null 2>&1
-
-  USER_KEYS=""
-  for PUB_KEY_FILE in ${USER_KEY_FILES}
-  do
-    _KEY="$(rt_cat_artifact "${RT_USERS_DIR}/${USER}${PUB_KEY_FILE}")"
-    NAME=
-    [[ ${KEYNAME} -eq 1 ]] && { NAME="${PUB_KEY_FILE//\//}: "; }
-    USER_KEYS="$(printf '%s\n%s' "${USER_KEYS}" "${NAME}${_KEY}")"
-  done
-
-  echo "${USER_KEYS}"
-}
-
-# Description:
-# Adds a user public key in metal3/users/<username>/<key_name>
-#
-# Credentials should be set in the following environment variables.
-#   RT_USER: artifactory user name.
-#   RT_TOKEN: artifactory password or api token
-#
-# Usage:
-#   rt_add_user_public_key <user> <public_key_name> <public_key_file>
-#
-rt_add_user_public_key() {
-
-  USER="${1:?}"
-  USER_PUB_KEY_NAME="${2:?}"
-  USER_PUB_KEY="${3:?}"
-
-  USER_PUB_KEY_FILE="$(mktemp)"
-  cat "${USER_PUB_KEY}" > "${USER_PUB_KEY_FILE}"
-
-  rt_upload_artifact "${USER_PUB_KEY_FILE}" "${RT_USERS_DIR}/${USER}/${USER_PUB_KEY_NAME}" 0
-  rm -f "${USER_PUB_KEY_FILE}"
-}
-
-# Description:
-# Deletes a user public key in metal3/users/<username>/<key_name>
-#
-# Credentials should be set in the following environment variables.
-#   RT_USER: artifactory user name.
-#   RT_TOKEN: artifactory password or api token
-#
-# Usage:
-#   rt_del_user_public_key <user> <public_key_name>
-#
-rt_del_user_public_key() {
-
-  USER="${1:?}"
-  USER_PUB_KEY_NAME="${2:?}"
-
-  USER_KEY_STAT="$(rt_list_directory "${RT_USERS_DIR}/${USER}/${USER_PUB_KEY_NAME}" \
-    | jq -r '.uri')" > /dev/null 2>&1
-
-  if [ "${USER_KEY_STAT}" != "null" ]
-  then
-    rt_delete_artifact "${RT_USERS_DIR}/${USER}/${USER_PUB_KEY_NAME}" 0
-  else
-    common_verbose "No such key: ${RT_USERS_DIR}/${USER}/${USER_PUB_KEY_NAME}"
-    return
-  fi
-
-  # Delete the user directory if there are no more keys
-  KEYS="$(rt_get_user_public_keys "${USER}")"
-
-  # Dry-mode will not detect an empty directory because the key is
-  # still in the artifactory
-  if [ -z "${KEYS}" ]
-  then
-    rt_delete_artifact "${RT_USERS_DIR}/${USER}" 0
-  else
-    common_verbose "Retaining artifact ${RT_USERS_DIR}/${USER}"
-  fi
 }
